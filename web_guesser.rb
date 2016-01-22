094e909f0c0d95930ed7978dab0a5e3beedcaa56
Chad Ellison <chad.ellison0123@gmail.com>
2016-01-22 14:02:46 -0700

    iter-4

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  response = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :response => response}
+end
+
+def check_guess(guess)
+  if params["guess"].to_i - 5 > SECRET_NUMBER
+    response = "Your guess is Way Too high!"
+  elsif params["guess"].to_i > SECRET_NUMBER
+    response = "Your guess is Too high!"
+  elsif params["guess"].to_i + 5 < SECRET_NUMBER
+    response = "Your guess is Way Too low!"
+  elsif params["guess"].to_i < SECRET_NUMBER
+    response = "Your guess is Too low!"
+  else
+    response = "You got it right! THE SECRET NUMBER IS #{SECRET_NUMBER}"
+  end
 end
