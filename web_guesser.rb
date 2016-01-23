df5b55e0367574c20b678888de448a8779d1e5eb
Chad Ellison <chad.ellison0123@gmail.com>
2016-01-22 17:21:28 -0700

    web_guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,23 +3,33 @@ require 'sinatra/reloader'
 require 'pry'
 
 SECRET_NUMBER = rand(100)
+@@color = "white"
+@@guesses_left = 5
 
 get '/' do
   guess = params["guess"].to_i
   response = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :response => response}
+
+  erb :index, :locals => {:number => SECRET_NUMBER,
+                          :response => response,
+                          :color => @@color}
 end
 
 def check_guess(guess)
-  if params["guess"].to_i - 5 > SECRET_NUMBER
+  if guess - 5 > SECRET_NUMBER
+    @@color = "red"
     response = "Your guess is Way Too high!"
-  elsif params["guess"].to_i > SECRET_NUMBER
+  elsif guess > SECRET_NUMBER
+    @@color = "pink"
     response = "Your guess is Too high!"
-  elsif params["guess"].to_i + 5 < SECRET_NUMBER
+  elsif guess + 5 < SECRET_NUMBER
+    @@color = "red"
     response = "Your guess is Way Too low!"
-  elsif params["guess"].to_i < SECRET_NUMBER
+  elsif guess < SECRET_NUMBER
+    @@color = "pink"
     response = "Your guess is Too low!"
   else
+    @@color = "green"
     response = "You got it right! THE SECRET NUMBER IS #{SECRET_NUMBER}"
   end
 end
