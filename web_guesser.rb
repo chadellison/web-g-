ef5219857bb0e43db4b9b7e5bbaaff905fad3e29
Chad Ellison <chad.ellison0123@gmail.com>
2016-01-23 13:45:30 -0700

    web guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,33 +3,40 @@ require 'sinatra/reloader'
 require 'pry'
 
 SECRET_NUMBER = rand(100)
+@@secret_number = SECRET_NUMBER
 @@color = "white"
 @@guesses_left = 5
 
 get '/' do
   guess = params["guess"].to_i
   response = check_guess(guess)
-
-  erb :index, :locals => {:number => SECRET_NUMBER,
-                          :response => response,
-                          :color => @@color}
+  if @@guesses_left > 0
+     @@guesses_left -= 1
+  else
+    @@guesses_left = 5
+    @@secret_number = rand(100)
+    response = "You lost. The number has been reset"
+  end
+  erb :index, :locals => {:number => @@secret_number,
+                        :response => response,
+                        :color => @@color}
 end
 
 def check_guess(guess)
-  if guess - 5 > SECRET_NUMBER
+  if guess - 5 > @@secret_number
     @@color = "red"
     response = "Your guess is Way Too high!"
-  elsif guess > SECRET_NUMBER
+  elsif guess > @@secret_number
     @@color = "pink"
     response = "Your guess is Too high!"
-  elsif guess + 5 < SECRET_NUMBER
+  elsif guess + 5 < @@secret_number
     @@color = "red"
     response = "Your guess is Way Too low!"
-  elsif guess < SECRET_NUMBER
+  elsif guess < @@secret_number
     @@color = "pink"
     response = "Your guess is Too low!"
   else
     @@color = "green"
-    response = "You got it right! THE SECRET NUMBER IS #{SECRET_NUMBER}"
+    response = "You got it right! THE SECRET NUMBER IS #{@@secret_number}"
   end
 end
